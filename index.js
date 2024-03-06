a519b42e25e68ca0a34bdac47008fae70772bafe
Mayur <mayurkanade59@gmail.com>
2024-03-06 17:34:22 +0530

    A complete node app docker image created.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+// express framework will take a request and send the response
 app.get("/", (req, res) => {
   res.json({ message: "Hello From Mayur Docker. 🐳" });
 });
 
+// application will be listened inside the container on this given port
 app.listen(9000, () => console.log("Server is running at port 9000"));
